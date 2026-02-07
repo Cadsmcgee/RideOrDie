72b13b50c12e93850d3e64dee3612f6f24b5797c
Rylan Champion <rylanchamp12@gmail.com>
2026-02-07 17:17:30 -0600

    pull from main

---
+++ Source/Gam305_GreenTeam/UpgradeRandomizer.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAM305_GREENTEAM_API UpgradeRandomizer
+{
+public:
+	UpgradeRandomizer();
+	~UpgradeRandomizer();
+};
